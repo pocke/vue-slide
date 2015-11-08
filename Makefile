1a99cde15b573a516e9ad234aba626795194c175
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-08 17:22:26 +0900

    html

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 all:
 	go get github.com/pocke/tsconfig-updater
 	shopt -s globstar && tsconfig-updater src/**/*.ts
+	go get github.com/pocke/js-file2string
+	js-file2string < src/index.html > dist/index.html.js
 	tsc
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 /// <reference path="../typings/bundle.d.ts" />
 
+import html from './index.html';
 import Vue = require('vue');
+
+class Slide extends Vue {
+  constructor(markdown: string) {
+    super({
+      el: html,
+    });
+  }
+}
