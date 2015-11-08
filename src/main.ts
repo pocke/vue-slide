3fd2abd95b7c042fbc59909d7d911da2af029dd2
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-08 18:37:16 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 /// <reference path="../typings/bundle.d.ts" />
 
-import html from './index.html';
+import html        from './index.html';
+import * as Marked from 'marked';
+
 import Vue = require('vue');
 
 class Slide extends Vue {
-  constructor(markdown: string) {
-    super({
+  constructor(private markdown: string) {
+    super();
+    this._init({
       el: html,
+      data: {
+        markdown: markdown,
+      },
+      computed: {
+        marked: this.marked,
+      },
     });
+  }
+
+  marked(): string {
+    return Marked(this.markdown);
   }
 }
